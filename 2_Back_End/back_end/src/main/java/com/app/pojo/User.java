430571096154c28342c1572dbb3f27177c4b823b
mahendra kolhe <mkolhe23@gmail.com>
2023-02-19 16:06:35 +0530

    users table dairy_express

--- 2_Back_End/back_end/src/main/java/com/app/pojo/User.java
+++ 2_Back_End/back_end/src/main/java/com/app/pojo/User.java
@@ -1,13 +1,92 @@
 package com.app.pojo;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "user")
-public class User {
+@Table(name = "users")
+public class User extends BaseEntity {
+	@Column(length = 20)
 	private String firstName;
+	@Column(length = 20)
 	private String lastName;
+	@Column(length = 20, unique = true)
 	private String email;
+	@Column(length = 20, nullable = false)
 	private String password;
+	@Column(length = 20)
+	private String phone;
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20)
+	private Role role;
+
+	public User() {
+	}
+
+	public User(String firstName, String lastName, String email, String password, String phone, Role role) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.phone = phone;
+		this.role = role;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "User Id+ " + getId() + " [firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", password=" + password + ", phone=" + phone + ", role=" + role + "]";
+	}
 }
