92d8c3b179133b3d0c3348d59d2e43f4cd7f4e64
mahendra kolhe <mkolhe23@gmail.com>
2023-02-19 19:35:21 +0530

    home page react

--- 1_Front_End/front_end/src/App.js
+++ 1_Front_End/front_end/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './Component/Navbar';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Dairy Express</h1>
+      <Navbar></Navbar>
     </div>
   );
 }
