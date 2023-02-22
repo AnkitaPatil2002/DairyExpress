38522925feb8c1db5d7015674f1d8239ad27e349
mahendra kolhe <mkolhe23@gmail.com>
2023-02-22 19:43:26 +0530

    spring boot dairy_express backend

--- 2_Back_End/back_end/src/main/java/com/app/pojo/Order.java
+++ 2_Back_End/back_end/src/main/java/com/app/pojo/Order.java
@@ -12,7 +12,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.Min;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
