264137ce5bbc149bbfd551967ab6f6901080d3a2
mahendra kolhe <mkolhe23@gmail.com>
2023-04-08 14:09:38 +0530

    jwt change

--- 2_Back_End/Dairy-Express-Backend-Spring/src/main/java/com/app/util/JwtUtil.java
+++ 2_Back_End/Dairy-Express-Backend-Spring/src/main/java/com/app/util/JwtUtil.java
@@ -57,7 +57,10 @@ public class JwtUtil implements Serializable {
 	}
 
 	private String doGenerateToken(Map<String, Object> claims, String subject) {
-		return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+		return Jwts.builder()
+				.setClaims(claims)
+				.setSubject(subject)
+				.setIssuedAt(new Date(System.currentTimeMillis()))
 				.setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
 				.signWith(SignatureAlgorithm.HS256, secret).compact();
 	}
